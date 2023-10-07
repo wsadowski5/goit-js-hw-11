d4a40a268a5bcf1e34493e88158a2386ad9e960d
wsadowski5 <130093777+wsadowski5@users.noreply.github.com>
2023-10-07 15:03:00 +0200

    firest task

--- src/index.js
+++ src/index.js
@@ -1,49 +1,25 @@
-import axios from "axios";
-
-axios.defaults.headers.common["x-api-key"] = "39805913-a4bc2a6c03690a5e9014989d5";
-
-
-const searchInput = document.querySelector('input')
-const searchButton = document.querySelector('button')
-
-const url = 'https://pixabay.com/api/';
+import { fetchImages } from "./partials/pixabay-api";
+const input = document.querySelector('input')
+const form = document.querySelector('form')
 
 
+let query =  input.value
+let page = 1
 
-//------------------ axios fetch ------------------
-
-
-const fetchImages = async (query, page) => {
-    try {
-        const response = await axios.get(url, {
-            params: {
-                key : '39805913-a4bc2a6c03690a5e9014989d5',
-    q : query, 
-    //  localStorage.getItem('searchTerms'),
-    image_type : 'photo',
-    orientation : 'horizontal',
-    safesearch : true,
-    page: page,
-    per_page: 20,
-            },
-        });
-        console.log(response.data)
-    } catch (error) {
-        console.error('ERROR fatch:', error);
-        throw error;
-    }
-}
-
-
-searchInput.addEventListener('change', (event) => {
-    localStorage.setItem('searchTerms', event.currentTarget.value)
-    })
-
-searchButton.addEventListener('click', (event) =>{
+form.addEventListener('submit', (event) =>{
     event.preventDefault();
-    const query = localStorage.getItem('searchTerms');
-    const page = 1;
-    if (query )
+    const newQuery = input.value;
+    if (newQuery === query ) {
+                page ++
+            }
+            else {
+                query = newQuery
+                page = 1;
+            } 
     fetchImages(query,page);
 }
 )
+
+
+
+
--- src/partials/pixabay-api.js
+++ src/partials/pixabay-api.js
@@ -1,41 +1,23 @@
 import axios from "axios";
+const url = 'https://pixabay.com/api/';
 
-axios.defaults.headers.common["x-api-key"] = "39805913-a4bc2a6c03690a5e9014989d5";
-
-
-const searchInput = document.querySelector('input')
-const searchButton = document.querySelector('button')
-
-const urlBase = 'https://pixabay.com/api/';
-
-const searchParams  = new URLSearchParams({
-    key : '39805913-a4bc2a6c03690a5e9014989d5',
-    q : localStorage.getItem('searchTerms'),
-    image_type : 'photo',
-    orientation : 'horizontal',
-    safesearch : true,
-    page: 1,
-    per_page: 20,
-})
-
-const url = `${urlBase}?${searchParams}`;
 
 
 //------------------ axios fetch ------------------
 
 
-const fetchImages = async (query, page) => {
+export const fetchImages = async (query, page) => {
     try {
         const response = await axios.get(url, {
             params: {
                 key : '39805913-a4bc2a6c03690a5e9014989d5',
     q : query, 
-    //  localStorage.getItem('searchTerms'),
     image_type : 'photo',
     orientation : 'horizontal',
     safesearch : true,
     page: page,
-    per_page: 20,
+    per_page: 40,
+   
             },
         });
         console.log(response.data)
@@ -44,17 +26,3 @@ const fetchImages = async (query, page) => {
         throw error;
     }
 }
-
-
-searchInput.addEventListener('change', (event) => {
-    localStorage.setItem('searchTerms', event.currentTarget.value)
-    })
-
-searchButton.addEventListener('click', (event) =>{
-    event.preventDefault();
-    searchParams.q = localStorage.getItem('searchTerms');
-    fetchImages(cat,1);
-}
-)
-
-console.log('olo')
