1f0ee638c9032a115d80c6257d3796b88729fb35
wsadowski5 <130093777+wsadowski5@users.noreply.github.com>
2023-10-08 15:38:28 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,67 +1,100 @@
 
 
-
+import Notiflix from "notiflix";
 import { fetchImages } from "./partials/pixabay-api";
 
 const input = document.querySelector('input')
 const form = document.querySelector('form')
 const gallery = document.querySelector('.gallery')
+const loadMoreBtn = document.querySelector('.load-more')
 
 
-
-let query =  input.value
+loadMoreBtn.classList.add('is-hidden')
+let query =  ''
 let page = 1
+let perPage = 4
+
+fetchImages(query,page)
 
 function clearGallery() {
     gallery.innerHTML = '';
   }
 
 
-function createGallery (event) {
+function renderGallery (event) {
     event.preventDefault();
-
-
-
+    clearGallery();
+    
     const newQuery = input.value;
+    
     if (newQuery === query ) {
                 page ++
+                
             }
             else {
                 query = newQuery
                 page = 1;
             } 
-    fetchImages(query,page)
-    .then(data => {
-        console.log(data)
-
-        clearGallery()
-        for (let i= 0; i <= data.hits.length; i++)
-        {
-            const markup = `<div class="photo-card">
-            <img src="${data.hits[i].webformatURL}" alt="${data.hits[i].tags}" loading="lazy" />
-            <div class="info">
-              <p class="info-item">
-                <b>Likes: ${data.hits[i].likes}</b>
-              </p>
-              <p class="info-item">
-                <b>Views: ${data.hits[i].views}</b>
-              </p>
-              <p class="info-item">
-                <b>Comments: ${data.hits[i].comments}</b>
-              </p>
-              <p class="info-item">
-                <b>Downloads: ${data.hits[i].downloads}</b>
-              </p>
-            </div>
-          </div>`
-          gallery.innerHTML += markup
+                
+    fetchImages(query,page,perPage)
+    .then(images => {
+      console.log(images)
+      if (images.totalHits === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        createGallery(images);
+        Notiflix.Notify.success(`Hooray! We found ${images.totalHits} images.`);
+        if (images.totalHits > perPage) {
+          loadMoreBtn.classList.remove('is-hidden');
         }
-    }
-)
-.catch (error => {
-    console.log(error)
-})
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+function loadMoreImages(images) {
+  page += 1
+
+  fetchImages(query,page,perPage)
+  .then(images =>{
+    createGallery(images);
+    // if ()
+  })
 }
 
 
-form.addEventListener('submit', createGallery)
+function createGallery(images) {
+  const markup = images.hits
+  .map(({webformatURL,largeImageURL,tags,likes,comments,views,downloads}) => {
+    return `<div class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes: ${likes}</b>
+          </p>
+          <p class="info-item">
+            <b>Views: ${views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments: ${comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads: ${downloads}</b>
+          </p>
+        </div>
+      </div>`
+      })
+      .join("")
+      gallery.insertAdjacentHTML('beforeend',markup)
+      const displayedHits = document.getElementsByClassName('info');
+      if(displayedHits < 500){
+        console.log ('abba')
+      }
+      
+    } 
+
+loadMoreBtn.addEventListener('click', loadMoreImages);
+
+form.addEventListener('submit', renderGallery);
--- src/partials/pixabay-api.js
+++ src/partials/pixabay-api.js
@@ -6,25 +6,25 @@ const url = 'https://pixabay.com/api/';
 //------------------ axios fetch ------------------
 
 
-export const fetchImages = async (query, page) => {
+export const fetchImages = async (query, page, perPage) => {
     try {
         const response = await axios.get(url, {
             params: {
                 key : '39805913-a4bc2a6c03690a5e9014989d5',
-    q : query, 
-    image_type : 'photo',
-    orientation : 'horizontal',
-    safesearch : true,
-    page: page,
-    per_page: 40,
+                q : query, 
+                image_type : 'photo',
+                orientation : 'horizontal',
+                safesearch : true,
+                page: page,
+                per_page: perPage,
    
             },
         });
+        return response.data
         
-        let data = response.data
-        return data
     } catch (error) {
         console.error('ERROR:', error);
         throw error;
     }
 }
+
